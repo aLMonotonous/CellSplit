d573a4866c1089e135841d6ccd0186f63ce0c15e
aL <al_0825@outlook.com>
2019-01-15 19:40:13 +0800

    rpn

--- rcnn/rpn.py
+++ rcnn/rpn.py
@@ -5,11 +5,28 @@ from display import get_img_annotation
 
 img_width, img_height = (480, 640)
 # note that down scale means the ratio along singe single height or width
-down_scale = 1/4
+down_scale = 1 / 4
 # 40,30
 anchor_ratios = [1, 0.5, 1.5]
 anchor_sizes = [128., 512., 2048.]
 iou_label_threshold = 0.5
+
+
+def union(au, bu):
+    area_a = (au[2] - au[0]) * (au[3] - au[1])
+    area_b = (bu[2] - bu[0]) * (bu[3] - bu[1])
+    area_union = area_a + area_b - intersection(au, bu)
+    return area_union
+
+
+def intersection(ai, bi):
+    x = max(ai[0], bi[0])
+    y = max(ai[1], bi[1])
+    w = min(ai[2], bi[2]) - x
+    h = min(ai[3], bi[3]) - y
+    if w < 0 or h < 0:
+        return 0
+    return w * h
 
 
 def get_random_clr():
@@ -34,15 +51,21 @@ def cal_rpn_y(boxes, w, h, dscale, ratios, sizes, detail=False):
             feature map size * boxes per point * 2
     """
     dboxes = []
-    dw = int(w*dscale)
-    dh = int(h*dscale)
+
+    dw = int(w * dscale)
+    dh = int(h * dscale)
+
+    n_boxes = len(boxes)
+    best_iou_bbox = np.zeros(n_boxes).astype('float32')
+    best_anchor_bbox = np.zeros([n_boxes, 4]).astype('int')
+
     for ibox in boxes:
         # no need for class label
         ibox = np.array(ibox[1:]).astype('float32')
-        dboxes.append(ibox*dscale)
+        dboxes.append(ibox * dscale)
     if detail:
         # for detail study
-        print(dboxes)
+        # print(dboxes)
         canv = np.zeros((dw, dh, 3), dtype="uint8")
         for ibox in dboxes:
             cv2.rectangle(canv, (ibox[0], ibox[1]), (ibox[2], ibox[3]), (0, 255, 0))
@@ -52,8 +75,8 @@ def cal_rpn_y(boxes, w, h, dscale, ratios, sizes, detail=False):
             clr = get_random_clr()
             for isize in anchor_sizes:
                 cnt = 0
-                ah = int(np.sqrt(iratio*isize))
-                aw = int(isize/ah)
+                ah = int(np.sqrt(iratio * isize))
+                aw = int(isize / ah)
                 for ix in range(dh):
                     axmin = int(ix - ah / 2)
                     axmax = int(ix + ah / 2)
@@ -68,11 +91,12 @@ def cal_rpn_y(boxes, w, h, dscale, ratios, sizes, detail=False):
                             continue
                         cnt += 1
                         # if cnt < 2:
-                        if ix == iy == min(dh, dw)//2:
+                        if ix == iy == min(dh, dw) // 2:
                             # print((axmin, aymin), (axmax, aymax))
-                            cv2.rectangle(canv, (axmin, aymin), (axmax, aymax), clr)
-        cv2.imshow('BBox Downscale', canv)
-        cv2.waitKey(0)
+                            pass
+                            # cv2.rectangle(canv, (axmin, aymin), (axmax, aymax), clr)
+
+
     for iratio in ratios:
         for isize in anchor_sizes:
             ah = int(np.sqrt(iratio * isize))
@@ -89,14 +113,27 @@ def cal_rpn_y(boxes, w, h, dscale, ratios, sizes, detail=False):
                     aymax = int(iy + aw / 2)
                     if aymin < 0 or aymax > w:
                         continue
+                    cur_anchor = [axmin, aymin, axmax, aymax]
+                    # cal IoU for every bbox
+                    for idx_dbox, ib in enumerate(dboxes):
+                        iou = intersection(cur_anchor, ib) / union(cur_anchor, ib)
+                        if iou > best_iou_bbox[idx_dbox]:
+                            best_iou_bbox[idx_dbox] = iou
+                            best_anchor_bbox[idx_dbox] = cur_anchor
+    if detail:
+        print(best_iou_bbox, best_anchor_bbox)
+        for b in best_anchor_bbox:
+            cv2.rectangle(canv, (b[0], b[1]), (b[2], b[3]), get_random_clr())
+
+    if detail:
+        cv2.imshow('BBox Downscale', canv)
+        cv2.waitKey(0)
+
+
+
+
 
 
 # cal_rpn_y(boxes, w, h, dscale, ratios, sizes)
 (img, test_boxex) = get_img_annotation(1, root='../')
-cal_rpn_y(test_boxex, img_width, img_height, down_scale, anchor_ratios, anchor_sizes,)
-
-
-
-
-
-
+cal_rpn_y(test_boxex, img_width, img_height, down_scale, anchor_ratios, anchor_sizes,True )
