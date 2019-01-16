3a918787af4283255012e4216c20ca26546b17cc
aL <al_0825@outlook.com>
2019-01-16 21:20:51 +0800

    anchor select

--- rcnn/rpn.py
+++ rcnn/rpn.py
@@ -1,3 +1,5 @@
+import itertools
+
 import cv2
 import numpy as np
 
@@ -35,9 +37,63 @@ def get_random_clr():
     return list(np.random.random(size=3) * 256)
 
 
+def corner2centre(corner_rprt):
+    """
+    change corner representation to  centre repr.
+    :param corner:array (xmin, ymin, xmax, ymax)
+    :return:
+    """
+    (xmin, ymin, xmax, ymax) = corner_rprt
+    cx = xmin + xmax // 2.0
+    cy = ymin + ymax // 2.
+    height = xmax - xmin
+    width = ymax - ymin
+    return cx, cy, height, width
+
+
+def cal_rgr_target(gtbox, anchor):
+    acx, acy, ach, acw = corner2centre(anchor)
+    boxcx, boxcy, boxh, boxw = corner2centre(gtbox)
+    rgr_cx = (boxcx - acx + 0.0) / ach
+    rgr_cy = (boxcy - acy + 0.0) / acw
+    rgr_h = np.log(boxh + 0.0 / ach)
+    rgr_w = np.log(boxw + 0.0 / acw)
+    # rgr_h = boxh + 0.0 / ach
+    # rgr_w = boxw + 0.0 / acw
+    return rgr_cx, rgr_cy, rgr_h, rgr_w
+
+
+def cal_anchor_index(x, y, idx_sizes, index_ratio, lx, ly, lis, lir):
+    # idx =
+    pass
+
+
+def reverse_anchor_shape(index, ratios, sizes):
+    """
+    reverse shape from index
+    :param index: [x,y,ratio+size*len(ratio)]
+    :param ratios:
+    :param sizes:
+    :return:
+    """
+    cx = index[0]
+    cy = index[1]
+    ratio = ratios[index[2] % len(ratios)]
+    size = sizes[index[2] // len(ratios)]
+    h = int(np.sqrt(ratio * size))
+    w = int(size / h)
+    xmin = cx - h // 2
+    xmax = cx + h // 2
+    ymin = cy - w // 2
+    ymax = cy + 2 // 2
+    return xmin, ymin, xmax, ymax
+
+
 def cal_rpn_y(boxes, w, h, dscale, ratios, sizes, detail=False):
     """
      calculate training target for region proposal network
+     anchor order: x,y,size,ratio
+     anchor[x,y,size_num,ratio_num]
     :param boxes: ground truth bounding boxs, list-like
     :param w: image width
     :param h: image height
@@ -59,6 +115,9 @@ def cal_rpn_y(boxes, w, h, dscale, ratios, sizes, detail=False):
     best_iou_bbox = np.zeros(n_boxes).astype('float32')
     best_anchor_bbox = np.zeros([n_boxes, 4]).astype('int')
     best_rgr_bbox = np.zeros([n_boxes, 4]).astype('float32')
+
+    # n_anchors = dw * dh *
+    anchor_cls_target = -1 * np.ones([dh, dw, len(ratios) * len(sizes)])
 
     for ibox in boxes:
         # no need for class label
@@ -96,8 +155,8 @@ def cal_rpn_y(boxes, w, h, dscale, ratios, sizes, detail=False):
                             # print((axmin, aymin), (axmax, aymax))
                             pass
                             # cv2.rectangle(canv, (axmin, aymin), (axmax, aymax), clr)
-    for iratio in ratios:
-        for isize in anchor_sizes:
+    for idx_size, isize in enumerate(sizes):
+        for idx_ratio, iratio in enumerate(ratios):
             ah = int(np.sqrt(iratio * isize))
             aw = int(isize / ah)
             for ix in range(dh):
@@ -112,18 +171,36 @@ def cal_rpn_y(boxes, w, h, dscale, ratios, sizes, detail=False):
                     aymax = int(iy + aw / 2)
                     if aymin < 0 or aymax > w:
                         continue
+                    # anchor_index = cal_anchor_index()
                     cur_anchor = [axmin, aymin, axmax, aymax]
                     # cal IoU for every bbox
                     for idx_dbox, ib in enumerate(dboxes):
                         iou = intersection(cur_anchor, ib) / union(cur_anchor, ib)
-                        if iou > best_iou_bbox[idx_dbox]:
-                            best_iou_bbox[idx_dbox] = iou
-                            best_anchor_bbox[idx_dbox] = cur_anchor
+                        if iou > iou_label_threshold:
+                            # note that there could be bbox that has no anchor
+                            anchor_cls_target[ix, iy, idx_ratio + idx_size * len(ratios)] = 1
+                            if iou > best_iou_bbox[idx_dbox]:
+                                best_iou_bbox[idx_dbox] = iou
+                                best_anchor_bbox[idx_dbox] = cur_anchor
+                                best_rgr_bbox[idx_dbox] = (cal_rgr_target(ib, cur_anchor))
 
     if detail:
-        print(best_iou_bbox, best_anchor_bbox)
+        print("best iou box")
+        print(best_iou_bbox)
+        print("best anchor box")
+        print(best_anchor_bbox)
+        print(dboxes)
+        print("best rgr target")
+        print(best_rgr_bbox)
+        print("cls target")
+        print(anchor_cls_target)
         for b in best_anchor_bbox:
             cv2.rectangle(canv, (b[0], b[1]), (b[2], b[3]), get_random_clr())
+        shape = (dh, dw, len(ratios) * len(sizes))
+        for idx in itertools.product(*[range(s) for s in shape]):
+            if anchor_cls_target[idx] == 1.0:
+                b = reverse_anchor_shape(idx, ratios, sizes)
+                cv2.rectangle(canv, (b[0], b[1]), (b[2], b[3]), get_random_clr())
 
     if detail:
         cv2.imshow('BBox Downscale', canv)
@@ -131,5 +208,5 @@ def cal_rpn_y(boxes, w, h, dscale, ratios, sizes, detail=False):
 
 
 # cal_rpn_y(boxes, w, h, dscale, ratios, sizes)
-(img, test_boxex) = get_img_annotation(123, root='../')
+(img, test_boxex) = get_img_annotation(1, root='../')
 cal_rpn_y(test_boxex, img_width, img_height, down_scale, anchor_ratios, anchor_sizes, True)
