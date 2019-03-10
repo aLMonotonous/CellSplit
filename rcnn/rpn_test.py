7229755ffab5b788118355899b55845df59cb727
aL <al_0825@outlook.com>
2019-03-10 15:28:16 +0800

    complete rpn test ,add nms

--- rcnn/rpn_test.py
+++ rcnn/rpn_test.py
@@ -55,24 +55,122 @@ def apply_regr_np(X, T):
         return X
 
 
-def rpn_decode(cls_target, rgr_target, C, usr_rgr=True):
+def non_max_suppression_fast(boxes, probs, overlap_thresh=0.9, max_boxes=300):
+    # code used from here: http://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
+
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    np.testing.assert_array_less(x1, x2)
+    np.testing.assert_array_less(y1, y2)
+
+    # if the bounding boxes integers, convert them to floats --
+    # this is important since we'll be doing a bunch of divisions
+    if boxes.dtype.kind == "i":
+        boxes = boxes.astype("float")
+
+    # initialize the list of picked indexes
+    pick = []
+
+    # calculate the areas
+    area = (x2 - x1) * (y2 - y1)
+
+    # sort the bounding boxes
+    idxs = np.argsort(probs)
+
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the intersection
+
+        xx1_int = np.maximum(x1[i], x1[idxs[:last]])
+        yy1_int = np.maximum(y1[i], y1[idxs[:last]])
+        xx2_int = np.minimum(x2[i], x2[idxs[:last]])
+        yy2_int = np.minimum(y2[i], y2[idxs[:last]])
+
+        ww_int = np.maximum(0, xx2_int - xx1_int)
+        hh_int = np.maximum(0, yy2_int - yy1_int)
+
+        area_int = ww_int * hh_int
+
+        # find the union
+        area_union = area[i] + area[idxs[:last]] - area_int
+
+        # compute the ratio of overlap
+        overlap = area_int / (area_union + 1e-6)
+
+        # delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],
+                                               np.where(overlap > overlap_thresh)[0])))
+
+        if len(pick) >= max_boxes:
+            break
+
+    # return only the bounding boxes that were picked using the integer data type
+    boxes = boxes[pick].astype("int")
+    probs = probs[pick]
+    return boxes, probs
+
+
+def rpn_decode(cls_target, rgr_target, C, use_rgr=True):
     anchor_sizes = C.anchor_sizes
     anchor_ratios = C.anchor_ratios
     (rows, cols) = cls_target.shape[1:3]
-    A = np.zeros((4, rpn_layer.shape[1], rpn_layer.shape[2], rpn_layer.shape[3]))
+    A = np.zeros((4, cls_target.shape[1], cls_target.shape[2], cls_target.shape[3]))
     curr_layer = 0
     for isize in anchor_sizes:
         for iratio in anchor_ratios:
             ah = int(np.sqrt(iratio * isize))
             aw = int(isize / ah)
-            regr = regr_layer[0, :, :, 4 * curr_layer:4 * curr_layer + 4]
+            rgr = rgr_target[0, :, :, 4 * curr_layer:4 * curr_layer + 4]
+            rgr = np.transpose(rgr, (2, 0, 1))
             X, Y = np.meshgrid(np.arange(cols), np.arange(rows))
             A[0, :, :, curr_layer] = X - ah / 2
             A[1, :, :, curr_layer] = Y - aw / 2
             A[2, :, :, curr_layer] = ah
             A[3, :, :, curr_layer] = aw
-            if use_regr:
-                A[:, :, :, curr_layer] = apply_regr_np(A[:, :, :, curr_layer], regr)
+            if use_rgr:
+                A[:, :, :, curr_layer] = apply_regr_np(A[:, :, :, curr_layer], rgr)
+            # ensure values are reasonable
+            A[2, :, :, curr_layer] = np.maximum(1, A[2, :, :, curr_layer])
+            A[3, :, :, curr_layer] = np.maximum(1, A[3, :, :, curr_layer])
+            A[2, :, :, curr_layer] += A[0, :, :, curr_layer]
+            A[3, :, :, curr_layer] += A[1, :, :, curr_layer]
+            A[0, :, :, curr_layer] = np.maximum(0, A[0, :, :, curr_layer])
+            A[1, :, :, curr_layer] = np.maximum(0, A[1, :, :, curr_layer])
+            A[2, :, :, curr_layer] = np.minimum(cols - 1, A[2, :, :, curr_layer])
+            A[3, :, :, curr_layer] = np.minimum(rows - 1, A[3, :, :, curr_layer])
+
+            all_boxes = np.reshape(A.transpose((0, 3, 1, 2)), (4, -1)).transpose((1, 0))
+            all_probs = cls_target.transpose((0, 3, 1, 2)).reshape((-1))
+
+            x1 = all_boxes[:, 0]
+            y1 = all_boxes[:, 1]
+            x2 = all_boxes[:, 2]
+            y2 = all_boxes[:, 3]
+
+            idxs = np.where((x1 - x2 >= 0) | (y1 - y2 >= 0))
+
+            all_boxes = np.delete(all_boxes, idxs, 0)
+            all_probs = np.delete(all_probs, idxs, 0)
+
+            curr_layer += 1
+            result = non_max_suppression_fast(all_boxes, all_probs, overlap_thresh=overlap_thresh, max_boxes=max_boxes)[
+                0]
+
+            return result
 
 
 if __name__ == '__main__':
